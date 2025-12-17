7fdd2712d69ee45598a0eaef7ce628096e27a07b
user0157 <117390825+user0157@users.noreply.github.com>
2025-12-17 13:51:49 -0300

    Add files via upload

--- services/data_service.py
+++ services/data_service.py
@@ -1,1 +1,20 @@
+from models.data_model import db, DataRow
 
+def replace_data(data: list[dict]):
+    try:
+        db.session.query(DataRow).delete()
+
+        for row in data:
+            db.session.add(DataRow(payload=row))
+
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        raise e
+
+def get_all_data() -> list[dict]:
+    rows = DataRow.query.all()
+    return [r.payload for r in rows]
+
+def has_data() -> bool:
+    return DataRow.query.count() > 0
